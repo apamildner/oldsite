b6118c81fb0019b45746cbeeedadee2684f53af6
Papibarozza <mildner.arvid@gmail.com>
2015-01-27 12:40:38 +0100

    Responsive menu live

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -1,7 +1,22 @@
 
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 250){
+            toggleTopMenu();
+        }
+
+          if ($(window).scrollTop() < 250){
+            bringBackOriginalMenu();
+        }
+
+    });
+});
+
 
 function toggleTopMenu() {
+	$(".main-nav ul").animate().stop();
+	$(".main-nav ul").animate({ width:"100%", margin:0, height:"65px" }, 500);
 	$(".main-nav").css("padding","0px");
 	$(".shadow").css("visibility","hidden");
 
@@ -10,7 +25,7 @@ function toggleTopMenu() {
 	$(".main-nav ul i").css("margin-top","3px");
 	$(".main-nav ul").css("position","fixed");
 
-	$(".main-nav ul").animate({ width:"100%", margin:0, height:"65px" }, 200);
+	
 
 
 
@@ -20,6 +35,34 @@ function toggleTopMenu() {
 	};
 
 
-	//$('li').click(toggleTopMenu);
+
+	function bringBackOriginalMenu() {
+		$(".main-nav ul").animate().stop();
+	$(".main-nav").css("padding","40.5px");
+	$(".shadow").css("visibility","visible");
+	$(".main-nav ul").css({
+    "marginTop": "0",
+    "marginRight": "auto",
+    "marginBottom": "0",
+    "marginLeft": "auto"
+})
+
+	$(".main-nav ul").css("border-top-left-radius","5px");
+	$(".main-nav ul").css("border-top-right-radius","5px");
+	$(".main-nav ul li").css("margin-top","40px");
+	$(".main-nav ul i").css("margin-top","40px");
+	$(".main-nav ul").css("position","relative");
+
+	$(".main-nav ul").animate({ width:"70%", height:"150px" }, 000);
+
+
+
+
+
+
+	};
+
+
+	
 
 
--- js/scroll.js
+++ js/scroll.js
@@ -3,17 +3,31 @@
 $( document ).ready(function() {
 
 	 $('#about').click(function(e){
+	 	
+
+	 		e.preventDefault();
       
      
+
      $("html, body").animate({ scrollTop: 1800 }, 1300);
     });
 
 
 	  $('#portfolio').click(function(e){
+	  		e.preventDefault();
 
       
      
      $("html, body").animate({ scrollTop: 3000 }, 1300);
+    });
+
+
+	   $('#home').click(function(e){
+	   	e.preventDefault();
+      
+     
+
+     $("html, body").animate({ scrollTop: 0 }, 1300);
     });
 });
 
